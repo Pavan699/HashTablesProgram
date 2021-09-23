ff7ef6a4bbecb6f65e46fd362679e11beebf5edf
Pavan699 <pavannakate03@gmail.com>
2021-09-23 08:54:06 -0700

    Adding Add and Get to find the frequency

---
+++ HashTableProgram/HashMapNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableProgram
+{
+    class HashMapNode<K, V>
+    {
+        public int size;
+        public LinkedList<HashKeyValue<K, V>>[] items;
+        public class HashKeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+        public HashMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<HashKeyValue<K, V>>[size];
+        }
+    
+        public int GetArrayIndex(K inputkey)
+        {
+            int index = inputkey.GetHashCode() % size;
+            return index;
+        }
+
+        public LinkedList<HashKeyValue<K, V>> GetLL(int index)
+        {
+            LinkedList<HashKeyValue<K, V>> llist = items[index];
+            if(llist == null)
+            {
+                llist = new LinkedList<HashKeyValue<K, V>>();
+                items[index] = llist;
+            }
+            return llist;
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayIndex(key);
+            LinkedList<HashKeyValue<K, V>> hashkeyvalue = GetLL(position);
+            foreach(HashKeyValue<K, V> item in hashkeyvalue)
+            {
+                if(item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayIndex(key);
+            LinkedList<HashKeyValue<K, V>> hashkeyvalue = GetLL(position);
+            HashKeyValue<K, V> item = new HashKeyValue<K, V> { Key = key, Value = value };
+            hashkeyvalue.AddLast(item);
+        }
+    }
+}
--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -7,6 +7,23 @@ namespace HashTableProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome HashTable Program");
+            HashMapNode<int, string> hashkv = new HashMapNode<int, string>(3);
+            hashkv.Add(0, "To");
+            hashkv.Add(1, "be");
+            hashkv.Add(2, "or");
+            hashkv.Add(3, "not");
+            hashkv.Add(4, "to");
+            hashkv.Add(5, "be");          
+
+            string value1 = hashkv.Get(5);
+            Console.WriteLine("5th Position Value is : "+value1);
+           
+            string value2 = hashkv.Get(3);
+            Console.WriteLine("3rd Position Value is : " + value2);
+
+            string value3 = hashkv.Get(0);
+            Console.WriteLine("0th Position Value is : " + value3);
+            
         }
     }
 }
